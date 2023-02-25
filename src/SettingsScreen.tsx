116d3fa7dd422cc41f2217a3ea19233885bc630e
gheorghe96 <curcagheorghe@gmail.com>
2023-02-25 11:03:19 +0200

    Init project on react native 0.71.3

---
+++ src/SettingsScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+export default function SettingsSCreen(): JSX.Element {
+  return <Text>"Settings Screen"</Text>;
+}
+
+const styles = StyleSheet.create({});
